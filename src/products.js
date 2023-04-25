836e1feb4dbdec8f893d75e27f892d962cfba520
khenn <kennedymbithi20@gmail.com>
2023-04-25 20:58:05 +0300

    innit

---
+++ src/products.js
@@ -0,0 +1,114 @@
+
+import product1 from './assets/jacket.jpg'
+import product2 from './assets/dress & top.jpg'
+import product3 from './assets/dress 1.jpg'
+import product4 from './assets/dress 2.jpg'
+import product5 from './assets/jacket & short.jpg'
+import product6 from './assets/jeans 1.jpg'
+import product7 from './assets/jeans 2.jpg'
+import product8 from './assets/jeans 3.jpg'
+import product9 from './assets/jeans 4.jpg'
+import product10 from './assets/jeans 5.jpg'
+import product11 from './assets/jeans and top.jpg'
+import product12 from './assets/jeans jacket.jpg'
+import product13 from './assets/leath,j top & jeans.jpg'
+import product14 from './assets/mummy  jeans.jpg'
+import product15 from './assets/tops.jpg'
+
+
+export const PRODUCTS = [
+    {
+    id: 1,
+    productName: "Jacket",
+    price: 999.0,
+    productImage: product1,
+
+},
+{
+    id: 2,
+    productName: "Dress",
+    price: 2000,
+    productImage: product2,
+},
+{
+    id: 3,
+    productName: "Dress",
+    price: 2000,
+    productImage: product3,
+},
+{
+    id: 4,
+    productName: "Dress",
+    price: 2000,
+    productImage: product4,
+},
+{
+    id: 5,
+    productName: "Jacket & short",
+    price: 2500,
+    productImage: product5,
+},
+{
+    id: 6,
+    productName: "Jeans",
+    price: 1200,
+    productImage: product6,
+},
+{
+    id: 7,
+    productName: "Jeans",
+    price: 1200,
+    productImage: product7,
+},
+{
+    id: 8,
+    productName: "Jeans",
+    price: 1200,
+    productImage: product8,
+},
+{
+    id: 9,
+    productName: "Jeans",
+    price: 1200,
+    productImage: product9,
+},
+{
+    id: 10,
+    productName: "Jeans",
+    price: 1200,
+    productImage: product10,
+},
+
+{
+    id: 11,
+    productName: "Jeans & top",
+    price: 2000,
+    productImage: product11,
+},
+{
+    id: 12,
+    productName: "Denim Jacket",
+    price: 1000,
+    productImage: product12,
+},
+{
+    id: 13,
+    productName: "Leather Jacket",
+    price: 2000,
+    productImage: product13,
+},
+{
+    id: 14,
+    productName: "Mummy Jeans",
+    price: 1500,
+    productImage: product14,
+},
+{
+    id: 15,
+    productName: "Top",
+    price: 600,
+    productImage: product15,
+},
+
+
+]
